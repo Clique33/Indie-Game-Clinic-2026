fcffda6537bc57e173e8b653abdfbcab2486946e
Clique33 <gabriegcc@gmail.com>
2026-05-14 14:05:52 -0300

    changed sprite for bonfire. bonfire can be lit.

--- indie-game-clinic-2026/entities/bonfire.gd
+++ indie-game-clinic-2026/entities/bonfire.gd
@@ -1,5 +1,10 @@
-extends Sprite2D
+extends AnimatedSprite2D
 class_name Bonfire
+
+
+@onready var lamp: Lamp = $Lamp
+@onready var flame_light: PointLight2D = $FlameLight
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +13,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	if not lamp:
+		return
+	
+	if lamp.is_turned_on and not flame_light.visible:
+		flame_light.visible = true
+		play("turned_on")
+	elif not lamp.is_turned_on and flame_light.visible:
+		flame_light.visible = false
+		play("turned_off")

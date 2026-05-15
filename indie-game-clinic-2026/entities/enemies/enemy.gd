b63fbb17a459fc60ba3ea082d4240b1e9288134f
Clique33 <gabriegcc@gmail.com>
2026-05-14 23:51:24 -0300

    added hitboxes to enemies and layed the foundation for killing player and running from light. added layer name for enemies not colliding with each other

--- indie-game-clinic-2026/entities/enemies/enemy.gd
+++ indie-game-clinic-2026/entities/enemies/enemy.gd
@@ -5,6 +5,11 @@ const LIGHTING_X_ORIGINAL : float = 0
 const LIGHTING_X_FLIPPED : float = 15
 const MOUTH_X_ORIGINAL : float = 2
 const MOUTH_X_FLIPPED : float = 4
+const HITBOX_X_ORIGINAL : float = -2
+const HITBOX_X_FLIPPED : float = 3
+
+
+signal killed_player
 
 
 @export var player : Player
@@ -13,6 +18,7 @@ const MOUTH_X_FLIPPED : float = 4
 @onready var sprite: AnimatedSprite2D = $Sprite
 @onready var lighting: Node2D = $Sprite/Lighting
 @onready var mouth_occluder: LightOccluder2D = $Sprite/Lighting/MouthOccluder
+@onready var hit_box: Area2D = $HitBox
 
 
 func _physics_process(delta: float) -> void:
@@ -34,11 +40,36 @@ func is_right() -> bool:
 				) < 90
 
 
+func run_away_from(danger : Node2D) -> void:
+	push_error("TODO")
+
+
+func stop_running_away() -> void:
+	push_error("TODO")
+
+
 func look_to(right : bool) -> void:
 	if right:
 		mouth_occluder.position.x = MOUTH_X_FLIPPED
 		lighting.position.x = LIGHTING_X_FLIPPED
+		hit_box.position.x = HITBOX_X_FLIPPED
 	else:
 		mouth_occluder.position.x = MOUTH_X_ORIGINAL
 		lighting.position.x = LIGHTING_X_ORIGINAL
+		hit_box.position.x = HITBOX_X_ORIGINAL
 	sprite.flip_h = right
+
+
+func _on_hit_box_area_entered(area: Area2D) -> void:
+	if area.get_parent() is Flame or area.get_parent() is Lamp:
+		run_away_from(area.get_parent())
+
+
+func _on_hit_box_area_exited(area: Area2D) -> void:
+	if area.get_parent() is Flame or area.get_parent() is Lamp:
+		stop_running_away()
+
+
+func _on_hit_box_body_entered(body: Node2D) -> void:
+	if body is Player:
+		killed_player.emit()
